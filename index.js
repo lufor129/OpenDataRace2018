8d180f595185431fc9aa21e295b16880341afef4
lufor129 <s59654655@gmail.com>
2018-05-14 07:57:19 +0800

    finish InfoWindow

--- index.js
+++ index.js
@@ -2,25 +2,174 @@ var map;
 var markers = [];
 var park_markers = [];
 var school_markers = [];
+var public_markers=[];
+var medical_markers=[];
 var infowindow;
-var position = [
-	{ lat: 22.610354, lng: 120.299564 },
-	{ lat: 22.615147, lng: 120.286689 },
-	{ lat: 22.615797, lng: 120.301141 },
-	{ lat: 22.615432, lng: 120.299999 }
-];
 
 var park_position = [
-	{ lat: 22.618480, lng: 120.287005 },
-	{ lat: 22.608853, lng: 120.297476 },
-	{ lat: 22.616317, lng: 120.299617 }
+	{ 
+		lat:22.618942, 
+		lng:120.288217,
+		"名稱":"真愛碼頭公園",
+		"地址":"803高雄市鹽埕區公園二路62號"
+	},
+	{ 
+		lat:22.623000, 
+		lng:120.275953,
+		"名稱":"哈瑪星鐵道文化園區",
+		"地址":"804高雄市鼓山區鼓山一路32號"
+	},
+	{ 
+		lat:22.624921, 
+		lng:120.287991,
+		"名稱":"228和平紀念公園",
+		"地址":"803高雄市鹽埕區中正四路"
+	},
+	{
+		lat:22.624554, 
+		lng:120.300082,
+		"名稱":"中央公園",
+		"地址":"801高雄市前金區中山路五福路"
+	},
+	{
+		lat:22.608826, 
+		lng:120.296777,
+		"名稱":"星光水岸公園",
+		"地址":"806高雄市前鎮區"
+	},
+	{
+		lat:22.607587, 
+		lng:120.309308,
+		"名稱":"勞工公園",
+		"地址":"806高雄市前鎮區中山三路"
+	},
+	{
+		lat:22.612162, 
+		lng:120.319114,
+		"名稱":"中正足球場",
+		"地址":"806高雄市前鎮區二聖一路290號"
+	},
+	{
+		lat:22.592590, 
+		lng:120.307055,
+		"名稱":"獅子公園",
+		"地址":"806高雄市前鎮區擴建一路43號"
+	}
 ]
 
 var school_position = [
-	{ lat: 22.623224, lng: 120.287833 },
-	{ lat: 22.623083, lng: 120.297790 },
-	{ lat: 22.606087, lng: 120.302081 },
-	{ lat: 22.586989, lng: 120.312037 }
+	{
+		lat:22.606128, 
+		lng:120.302050,
+		"名稱":"高雄市立成功啟智學校",
+		"地址":"806高雄市前鎮區復興三路3號",
+		"電話":"07 330 4624"
+	},
+	{
+		lat:22.626608, 
+		lng:120.265803,
+		"名稱":"國立中山大學",
+		"地址":"804高雄市鼓山區蓮海路70號",
+		"電話":"07 525 2000"
+	},
+	{
+		lat:22.622648, 
+		lng:120.288190, 
+		"名稱":"高雄市立鹽埕國民中學",
+		"地址":"803高雄市鹽埕區新樂街46號",
+		"電話":"07 521 1283"
+	},
+	{
+		lat:22.623831, 
+		lng:120.286919,
+		"名稱":"光榮國小",
+		"地址":"803高雄市鹽埕區大智路150號",
+		"電話":"07 551 4549"
+	},
+	{
+		lat:22.613313, 
+		lng:120.297219,
+		"名稱":"高雄市立成功國民小學",
+		"地址":"802高雄市苓雅區華新街59號",
+		"電話":"07 334 1882"
+	},
+	{
+		lat:22.610935, 
+		lng:120.323011,
+		"名稱":"光華國中",
+		"地址":"806高雄市前鎮區和平二路170號",
+		"電話":"07 722 2622"
+	},
+	{
+		lat:22.604714, 
+		lng:120.308034,
+		"名稱":"獅甲國中",
+		"地址":"806高雄市前鎮區中山三路43號",
+		"電話":"07 333 1522"
+	},
+]
+
+var public_position=[
+	{
+		lat:22.619926, 
+		lng:120.281695,
+		"名稱":"駁二藝術特區",
+		"地址":"803高雄市鹽埕區大勇路1號"
+	},
+	{
+		lat:22.615998, 
+		lng:120.292408,
+		"名稱":"海洋文化及流行音樂中心",
+		"地址":"802高雄市苓雅區海邊路"
+	},
+	{
+		lat:22.611714, 
+		lng:120.293320,
+		"名稱":"高雄港港埠旅運中心",
+		"地址":"802高雄市苓雅區"
+	},
+	{
+		lat:22.609871, 
+		lng:120.301946,
+		"名稱":"高雄市立圖書館總館",
+		"地址":"80661高雄市前鎮區新光路61號"
+	},
+	{
+		lat:22.608048, 
+		lng:120.299071,
+		"名稱":"高雄展覽館",
+		"地址":"806高雄市前鎮區成功二路39號"
+	},
+	{
+		lat:22.604303, 
+		lng:120.300594,
+		"名稱":"高雄展軟體園區",
+		"地址":"806高雄市前鎮區成功二路復興四路"
+	}
+]
+
+var medical_position=[
+	{
+		lat:22.615400, 
+		lng:120.297802,
+		"名稱":"阮綜合醫療社團法人阮綜合醫院",
+		"地址":"802高雄市苓雅區成功一路162號",
+		"電話":"07 335 1121"
+	},
+	{
+		lat:22.607803, 
+		lng:120.317709,
+		"名稱":"新正薪醫院",
+		"地址":"806高雄市前鎮區一心一路233號",
+		"電話":"07 970 5335"
+	},
+	{
+		lat:22.626850, 
+		lng:120.297367,
+		"名稱":"高雄市立大同醫院",
+		"地址":"801高雄市前金區中華三路68號",
+		"電話":"07 291 1101"
+	}
 ]
 
 var Target_area = [
@@ -132,13 +281,14 @@ function initMap() {
 	 });
 	 */
 }
-function addMarker(e, position, markers) {
+function addMarker(e, position, markers,markerpic) {
 	markers[e] = new google.maps.Marker({
 		position: {
 			lat: position[e].lat,
 			lng: position[e].lng
 		},
 		map: map,
+		icon:markerpic,
 		animation: google.maps.Animation.DROP
 	});
 
@@ -148,13 +298,28 @@ function addMarker(e, position, markers) {
 			infowindow.close();
 		}
 		infowindow = new google.maps.InfoWindow();
-		infowindow.setContent("<p>lat:" + position[e].lat + "</p>");
+		infowindow.setContent(InfoContent(position[e]));
 		infowindow.open(map, markers[e]);
 		markers[e].setAnimation(google.maps.Animation.BOUNCE);
 		setTimeout(function () {
 			markers[e].setAnimation(null)
 		}, 3000);
 	});
+}
+
+function InfoContent(marker_position){
+	var html="<div>";
+	for(var key in marker_position){
+		if(key=="lat"){
+			html+="<p>"+"經度"+" : "+marker_position[key]+"</p>";
+		}else if(key=="lng"){
+			html+="<p>"+"緯度"+" : "+marker_position[key]+"</p>";
+		}else{
+			html+="<p>"+key+" : "+marker_position[key]+"</p>";
+		}
+	}
+	html+="</div>";
+	return html;
 }
 
 function changeColor(area, opacity) {
@@ -177,11 +342,11 @@ function cleanMarker(markers) {
 }
 $(function () {
 	$("#park").change(function () {
-		var infowindow = new google.maps.InfoWindow();
+		var markerpic="http://maps.google.com/mapfiles/ms/icons/green-dot.png";
 		if (this.checked) {
 			cleanMarker(park_markers);
 			for (var i = 0; i < park_position.length; i++) {
-				addMarker(i, park_position, park_markers);
+				addMarker(i, park_position, park_markers,markerpic);
 			}
 		} else {
 			cleanMarker(park_markers);
@@ -189,17 +354,40 @@ $(function () {
 	});
 
 	$("#school").change(function () {
-
+		var markerpic="http://maps.google.com/mapfiles/ms/icons/yellow-dot.png";
 		if (this.checked) {
 			cleanMarker(school_markers);
 			for (var i = 0; i < school_position.length; i++) {
-				addMarker(i, school_position, school_markers);
+				addMarker(i, school_position, school_markers,markerpic);
 			}
 		} else {
 			cleanMarker(school_markers);
 		}
 	});
 
+	$("#public").change(function () {
+		var markerpic="http://maps.google.com/mapfiles/ms/icons/purple-dot.png";
+		if (this.checked) {
+			cleanMarker(public_markers);
+			for (var i = 0; i < public_position.length; i++) {
+				addMarker(i, public_position, public_markers,markerpic);
+			}
+		} else {
+			cleanMarker(public_markers);
+		}
+	});
+
+	$("#medical").change(function () {
+		var markerpic="http://maps.google.com/mapfiles/ms/icons/red-dot.png";
+		if (this.checked) {
+			cleanMarker(medical_markers);
+			for (var i = 0; i < medical_position.length; i++) {
+				addMarker(i, medical_position, medical_markers,markerpic);
+			}
+		} else {
+			cleanMarker(medical_markers);
+		}
+	});
 
 	$("#bar").change(function () {
 		var opacity = $(this).val() / 10;
